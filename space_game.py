a941837deaaedd746d56e97dc98e4beeedb9bb3f
devoden <freeker07dd@gmail.com>
2021-06-11 10:07:57 +0300

    Update README.md

--- space_game.py
+++ space_game.py
@@ -17,15 +17,22 @@ for k in range(n):
     y = randint(0, 495)
     canvas.create_oval(x, y, x + 1, y + 1, outline='white', fill='white')
 
+# shuttle 1 position
 shuttle1_img = PhotoImage(file='shuttle1.png')
-shuttle1_img = shuttle1_img.subsample(4, 4)
+shuttle1_img = shuttle1_img.subsample(3, 3)
 shuttle1 = canvas.create_image(300, 50, image=shuttle1_img, anchor=NW)
+
+# shuttle 2 position
+shuttle2_img = PhotoImage(file='shuttle2.png')
+shuttle2_img = shuttle2_img.subsample(3, 3)
+shuttle2 = canvas.create_image(150, 150, image=shuttle2_img, anchor=SW)
 
 
 def move_control(event):
     ascii_code = event.keycode
-    # print(ascii_code)
     d = 5
+
+    # arrow control shuttle1
     if ascii_code == 37:
         canvas.move(shuttle1, -d, 0)
     elif ascii_code == 39:
@@ -35,7 +42,18 @@ def move_control(event):
     elif ascii_code == 40:
         canvas.move(shuttle1, 0, d)
 
+    # WASD control shuttle2
+    if ascii_code == 87:  # w
+        canvas.move(shuttle2, 0, -d)
+    elif ascii_code == 83:  # s
+        canvas.move(shuttle2, 0, d)
+    elif ascii_code == 65:  # a
+        canvas.move(shuttle2, -d, 0)
+    elif ascii_code == 68:  # d
+        canvas.move(shuttle2, d, 0)
+
 
 canvas.bind('<Key>', move_control)
 canvas.focus_set()
+
 root.mainloop()
